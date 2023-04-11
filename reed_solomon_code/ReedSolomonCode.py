b7378ce882bf9128e31208abb63505f3f769d2af
mazak <michal.maziarz12@gmail.com>
2023-04-11 18:53:24 +0200

    Almost done, only Forney's algorithm to do

--- reed_solomon_code/ReedSolomonCode.py
+++ reed_solomon_code/ReedSolomonCode.py
@@ -63,6 +63,7 @@ class ReedSolomonCode:
                 total_word += self.__encode_message(message[k:k + max_bits_per_word])
                 k += max_bits_per_word
             return total_word
+
     def decode_number(self, message):
         max_bits_per_word = self.__m * self.__k
         if len(message) < max_bits_per_word:
@@ -116,8 +117,16 @@ class ReedSolomonCode:
         polynomial = self.__get_message_polynomial(message)
         polynomial[0] = 10
         syndromes = self.__calculate_syndrome_components(polynomial)
-        print(polynomial)
-        print(syndromes)
+        print('Polynomial: ', polynomial)
+        print('Syndromes: ', syndromes)
+        error_locator_polynomial = self.__berlekamps_massey(syndromes)
+        print('delta: ', error_locator_polynomial)
+        magnitude = self.__calculate_error_magnitude(syndromes, error_locator_polynomial)
+        print('magnitude ', magnitude)
+        error_locations = self.__chein_search(error_locator_polynomial)
+        print('roots: ', error_locations)
+
+
 
     def __get_message_polynomial(self, message):
         galois_polynomial = []
@@ -148,7 +157,8 @@ class ReedSolomonCode:
             if coef != 0:
                 for j in range(1, len(divisor)):
                     if divisor[j] != 0:
-                        result[i + j] = ReedSolomonCode.add_galois(result[i + j], self.__multiply_galois(divisor[j], coef))
+                        result[i + j] = ReedSolomonCode.add_galois(result[i + j],
+                                                                   self.__multiply_galois(divisor[j], coef))
 
         separator = -(len(divisor) - 1)
         return result[:separator], result[separator:]
@@ -171,3 +181,65 @@ class ReedSolomonCode:
         for i in range(1, 2 * self.__t + 1):
             syndromes.append(self.__calculate_polynomial(polynomial, self.__table[i]))
         return syndromes
+
+    def __berlekamps_massey(self, syndromes):
+        k = 1
+        l = 0
+        c_x = [1, 0]
+        delta_x = [1]
+        while k <= 2 * self.__t:
+            e = syndromes[k - 1]
+            for j in range(1, l + 1):
+                e = self.add_galois(e, self.__multiply_galois(delta_x[len(delta_x) - j - 1], syndromes[k - 1 - j]))
+            if e != 0:
+                delta_star = self.__add_two_polynomials(delta_x, self.__poly_multiply_by_scalar(c_x, e))
+                if 2 * l < k:
+                    l = k - l
+                    c_x = self.__poly_multiply_by_scalar(delta_x, self.__inverse_galois(e))
+                delta_x = delta_star
+            k += 1
+            c_x.append(0)
+        return delta_x
+
+    def __poly_multiply_by_scalar(self, polynomial, scalar):
+        multiplied = [0] * len(polynomial)
+        for i in range(0, len(polynomial)):
+            multiplied[i] = self.__multiply_galois(polynomial[i], scalar)
+        return multiplied
+
+    def __add_two_polynomials(self, p1, p2):
+        if len(p1) > len(p2):
+            greater = p1
+            lesser = [0] * (len(p1) - len(p2)) + p2
+        else:
+            greater = p2
+            lesser = [0] * (len(p2) - len(p1)) + p1
+        total = [0] * len(greater)
+        for i in range(0, len(greater)):
+            if i < len(lesser):
+                total[i] = self.add_galois(greater[i], lesser[i])
+            else:
+                total[i] = greater[i]
+        return total
+
+    def __inverse_galois(self, x):
+        alpha = self.__table.index(x)
+        return self.__table[((2 ** self.__m - 1) - alpha)]
+
+    def __calculate_error_magnitude(self, syndromes, locator):
+        reversed_syndromes = syndromes.copy()
+        reversed_syndromes.reverse()
+        s_times_delta = self.__multiply_poly_galois(reversed_syndromes, locator)
+        divisor = [1] + [0] * (self.__t * 2 - 1)
+        q, r = self.__galois_division(s_times_delta, divisor)
+        return r
+
+    def __chein_search(self, error_locator_polynomial):
+        roots = []
+        for x in self.__table:
+            if self.__calculate_polynomial(error_locator_polynomial, x) == 0:
+                roots.append(x)
+        return roots
+
+    def __forney_algorithm(self, error_magnitude, error_locator, roots):
+        pass
